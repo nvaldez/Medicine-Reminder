de530d3813d99307e388d149be39bea1df25ff32
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-04-12 11:26:55 -0400

    header

--- medicine-reminder/src/components/Header/Header.js
+++ medicine-reminder/src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../images/medicine.png';
 import './Header.css';
 
 class Header extends Component {
